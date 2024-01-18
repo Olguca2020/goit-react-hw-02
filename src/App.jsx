3be3559cf4a660f11e7e7ad3f2f22a64ce8ca16e
Shchyrba Olga <olga.binder@gmail.com>
2024-01-19 00:03:05 +0200

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { Notification } from './components/Notification/Notification';
 import { Description } from './components/Description/Description';
 import { Options } from './components/Options/Options';
 import { Feedback } from './components/Feedback/Fidback';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [values, setValues] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [values, setValues] = useState(() => {
+    const saveObject = window.localStorage.getItem('review-object');
+    if (saveObject !== null) {
+      return JSON.parse(saveObject);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
+
   const totalFeedback = values.good + values.neutral + values.bad;
   const ifReset = totalFeedback > 0;
   const handleClickReset = () => {
@@ -19,15 +26,17 @@ function App() {
       bad: 0,
     });
   };
+
+  useEffect(() => {
+    window.localStorage.setItem('review-object', JSON.stringify(values));
+  }, [values]);
   const handleClick = ev => {
     const type = ev.target.textContent;
 
-    {
-      setValues(values => ({
-        ...values,
-        [type]: values[type] + 1,
-      }));
-    }
+    setValues(values => ({
+      ...values,
+      [type]: values[type] + 1,
+    }));
   };
 
   return (
